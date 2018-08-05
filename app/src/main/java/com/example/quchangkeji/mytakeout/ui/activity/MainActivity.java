2ad110f77e8935c9801ab114c07e691926070d87
zhuwujing <zhuwujing726@163.com>
2018-08-05 20:50:39 +0800

    1018.8.5框架的搭建

--- app/src/main/java/com/example/quchangkeji/mytakeout/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/quchangkeji/mytakeout/ui/activity/MainActivity.java
@@ -4,8 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import com.example.quchangkeji.mytakeout.R;
+import com.example.quchangkeji.mytakeout.ui.base.BaseActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
