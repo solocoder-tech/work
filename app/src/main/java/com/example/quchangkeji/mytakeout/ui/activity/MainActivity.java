d366d5c706343dc61bd2fb544e26f8305b1c2ed7
zhuwujing <zhuwujing726@163.com>
2018-08-06 23:15:49 +0800

    底部Button的实现,这里没有用RadioGroup和RadioButton

--- app/src/main/java/com/example/quchangkeji/mytakeout/ui/activity/MainActivity.java
+++ app/src/main/java/com/example/quchangkeji/mytakeout/ui/activity/MainActivity.java
@@ -1,16 +1,80 @@
 package com.example.quchangkeji.mytakeout.ui.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 import com.example.quchangkeji.mytakeout.R;
 import com.example.quchangkeji.mytakeout.ui.base.BaseActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends BaseActivity {
 
+    @BindView(R.id.fragment_container)
+    FrameLayout mFragmentContainer;
+    @BindView(R.id.item_container)
+    LinearLayout mItemContainer;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public int getContentLayoutId() {
+        return R.layout.activity_main;
     }
+
+    @Override
+    protected void init() {
+        setLisenters();
+        //初始状态
+        onClickLisenter.onClick(mItemContainer.getChildAt(0));
+    }
+
+    /**
+     * 设置点击监听
+     */
+    private void setLisenters() {
+        int childCount = mItemContainer.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            mItemContainer.getChildAt(i).setOnClickListener(onClickLisenter);
+        }
+    }
+
+    /**
+     * 自己不可点击,其他同级别的可以点击
+     * 自己的子孙View也不可点击
+     */
+    private View.OnClickListener onClickLisenter = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int indexOfChild = mItemContainer.indexOfChild(v);
+            for (int i = 0; i < mItemContainer.getChildCount(); i++) {
+                if (indexOfChild == i) {
+                    mItemContainer.getChildAt(i).setEnabled(false);
+                    //自己的子孙View也不可点击
+                    setEnable(mItemContainer.getChildAt(i), false);
+                } else {
+                    mItemContainer.getChildAt(i).setEnabled(true);
+                    setEnable(mItemContainer.getChildAt(i), true);
+                }
+            }
+        }
+    };
+
+    /**
+     * 递归遍历子孙View
+     *
+     * @param childAt
+     * @param b
+     */
+    private void setEnable(View childAt, boolean b) {
+        childAt.setEnabled(b);
+        if (childAt instanceof ViewGroup) {
+            for (int i = 0; i < ((ViewGroup) childAt).getChildCount(); i++) {
+                setEnable(((ViewGroup) childAt).getChildAt(i), b);
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/quchangkeji/mytakeout/ui/base/BaseActivity.java
+++ app/src/main/java/com/example/quchangkeji/mytakeout/ui/base/BaseActivity.java
@@ -1,10 +1,39 @@
 package com.example.quchangkeji.mytakeout.ui.base;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by zhuwujing on 2018/8/4.
  */
 
-public class BaseActivity extends Activity {
+public abstract class BaseActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getContentLayoutId());
+        ButterKnife.bind(this);
+        init();
+    }
+
+    /**
+     * 子类必须实现,提供自己的布局
+     *
+     * @return
+     */
+    public abstract int getContentLayoutId();
+
+    /**
+     * 子类必须实现,提供自己的初始化
+     */
+    protected abstract void init();
+
+    protected void toast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
 }
